f2edd72a019eac36f185de293bde5e4ad64466bf
johnnytal <johnnytal9@gmail.com>
2019-03-12 12:58:21 +0200

    no bullshit

--- js/preloader.js
+++ js/preloader.js
@@ -2,11 +2,6 @@ var preloader = function(game){};
  
 preloader.prototype = {
     preload: function(){
-        game.load.audio('shakerGentle', 'assets/audio/shakerGentle.mp3');
-        game.load.audio('shakerStrong', 'assets/audio/shakerStrong.ogg');
-        game.load.audio('shakerBack', 'assets/audio/shakerBack.mp3');
-        game.load.audio('front', 'assets/audio/front.ogg');
-        game.load.audio('back', 'assets/audio/back.ogg');
         game.load.image('circle', 'assets/images/shaker.png');
         game.load.image('bg', 'assets/images/bg.png');
     },
--- js/shaker.js
+++ js/shaker.js
@@ -1,5 +1,4 @@
 var shakerMain = function(game){
-	//LOUD_COLOR = '#f1ead7';
 	GENTLE_COLOR = '#c1ad65';
 	BACK_COLOR = '#656d7c';
 
@@ -11,17 +10,10 @@ var shakerMain = function(game){
 	ballFactor = 969;
 
 	resetTouching = true;
-	
-	frontSfx = null;
-	backSfx = null;
 };
 
 shakerMain.prototype = {
     create: function(){
-    	
-    	window.plugins.NativeAudio.preloadSimple('frontSfx', 'assets/audio/shakerGentle.mp3', function(msg){}, function(msg){alert(msg)});
-    	window.plugins.NativeAudio.preloadSimple('backSfx', 'assets/audio/shakerBack.mp3', function(msg){}, function(msg){alert(msg)});
-
     	bg = game.add.image(0, 0, 'bg');
     	bg.alpha = 0.6;
     	
@@ -35,17 +27,9 @@ shakerMain.prototype = {
         circle.scale.set(ballFactor/1000, ballFactor/1000);
  
         circle.body.collideWorldBounds = true;
-        
-      /*  angleText = game.add.text(510, 20, "Shake It Baby!", {font: '26px', fill: 'white'});
-        
-        accelText = game.add.text(10, 10, "accel", {font: '26px', fill: 'white'});
-        gammaText = game.add.text(10, 50, "gamma", {font: '26px', fill: 'white'});
-        ballText = game.add.text(10, 90, "ball", {font: '26px', fill: 'white'});
-        loudText = game.add.text(10, 130, "vol", {font: '26px', fill: 'white'});*/
-		
+
 		loadSounds();
 		initPlugIns();
-		//createUI();
     },
     
     update: function(){
@@ -94,48 +78,6 @@ function flash(_color){
 	}, 200);
 }
 
-function createUI(){
-	var sliderAccel = document.getElementById("accel");
-	
-	accelText.text = 'Accel: ' + sliderAccel.value;
-
-	sliderAccel.oninput = function() {
-		accelText.text = 'Accel: ' + this.value;
-		accelFactor = this.value;
-	};
-	
-	var sliderGamma = document.getElementById("gamma");
-	
-	gammaText.text = 'Gamma: ' + sliderGamma.value;
-
-	sliderGamma.oninput = function() {
-		gammaText.text = 'Gamma: ' + this.value;
-		gammaFactor = this.value;
-	};
-		
-	var sliderBall = document.getElementById("ball");
-	
-	ballText.text = 'Ball: ' + sliderBall.value / 1000;
-
-	sliderBall.oninput = function() {
-		ballText.text = 'Ball: ' + this.value / 1000;
-		ballFactor = this.value / 1000;
-		
-		circle.scale.set(ballFactor, ballFactor);
-	};
-	
-	var sliderLoud = document.getElementById("loud");
-	
-	loudText.text = 'Vol: ' + sliderLoud.value / 1000;
-
-	sliderLoud.oninput = function() {
-		loudText.text = 'Vol: ' + this.value / 1000;
-		MIN_VOL = this.value / 1000;
-		LOUD_VOL = 1.3 + (MIN_VOL - 0.25);
-	};
-	
-}
-
 function roundIt(_num){
 	return Math.round(_num * 100) / 100;
 }
@@ -160,14 +102,10 @@ function initPlugIns(){
     try{window.androidVolume.setMusic(100, false);} catch(e){} // max media volume
 }
 
-function loadSounds(){
-	shakerGentle = game.add.audio('shakerGentle', 1, false);
-	shakerStrong = game.add.audio('shakerStrong', 1, false);
-	shakerBack = game.add.audio('shakerBack', 1, false);
+function loadSounds(){	
+	window.plugins.NativeAudio.preloadSimple('frontSfx', 'assets/audio/shakerGentle.mp3', 
+	null, function(msg){alert(msg);});
 	
-	snareFront = game.add.audio('front', 1, false);
-	snareBack = game.add.audio('back', 1, false);
-	
-	frontSfx = shakerGentle;
-	backSfx = shakerBack;
+	window.plugins.NativeAudio.preloadSimple('backSfx', 'assets/audio/shakerBack.mp3', 
+	null, function(msg){alert(msg);});
 }
